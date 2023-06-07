ba96c6600ba117d3e838506d38ddf2bc0cbba464
ChenRuLee <123973509+ChenRuLee@users.noreply.github.com>
2023-06-07 23:31:30 +0800

    Delete license4 directory

--- license4/price/price.js
+++
@@ -1,9 +0,0 @@
-// $( function() {
-//     $( ".accordion_card" ).accordion();
-//   } );
-
- document.querySelectorAll('.accordion').forEach(function(elem){
-        elem.querySelector('p').addEventListener('click',function(){  
-            elem.classList.toggle('open')
-        })
-    })
